1443c3405d790516e79fa7bdd15bd951c5a77ded
RiyasaatAhmed <rrahul171089@bscse.uiu.ac.bd>
2020-06-24 14:46:05 +0600

    Lazy Image Loading and Parallax Transition

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ const navSlide = () =>
 {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('#nav-bar img + ul');
-    const n = document.querySelector('.n');
     const mobileSize = window.matchMedia('(max-width : 500px)');
 
+
+
+
+    // Hambuger Menu
     if(mobileSize.matches){
 
         nav.addEventListener('click', () =>{
@@ -25,17 +28,9 @@ const navSlide = () =>
     });
 
 
-    window.addEventListener('scroll', () =>{
-        if(window.scrollY > 150){
-            n.style.opacity = 0.9;
-        }
-        else{
-            n.style.opacity = 1;
-        }
 
-    });
     
-    $('.n a').on('click', function(event) {
+    $('.nav-style a').on('click', function(event) {
         if(this.hash !== ''){
 
             event.preventDefault();
@@ -49,6 +44,59 @@ const navSlide = () =>
 
             );
         }
+    });
+
+    // Nav-bar Opacity 
+    const n = document.querySelector('.nav-style');
+    const sectionOne = document.querySelector('.sectionOne');
+    const sectionOneOptions = { rootMargin: "-700px 0px 0px 0px"};
+    const observerSectionOne = 
+        new IntersectionObserver((entries,observerSectionOne) =>{
+
+            entries.forEach(entry => {
+                if(!entry.isIntersecting)
+                {
+                    n.style.opacity = 0.9;
+                }
+                else
+                {
+                    n.style.opacity = 1;
+                }
+            });
+        }, sectionOneOptions)
+    observerSectionOne.observe(sectionOne);
+
+    // Parallax Transition
+    const parallax = document.querySelectorAll('.parallax');
+    const parallaxObserver = new IntersectionObserver((entries, parallaxObserver) => {
+
+        entries.forEach(entry =>{
+            if(entry.isIntersecting)
+            {
+                entry.target.classList.add('parallax-view');
+                parallaxObserver.unobserve(entry.target);
+
+            }
+        });
+    });
+    parallax.forEach(para => {
+        parallaxObserver.observe(para);
+    });
+
+    // Photos Transition
+    const photosOptions = { rootMargin: "-40px 0px 0px 0px" };
+    const photos = document.querySelectorAll('.photos');
+    const photoObserver = new IntersectionObserver((entries, photoObserver) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting)
+            {
+                entry.target.classList.add('photos-in-view');
+                photoObserver.unobserve(entry.target);
+            }
+        });
+    }, photosOptions);
+    photos.forEach(photo => {
+        photoObserver.observe(photo);
     });
 }
 
